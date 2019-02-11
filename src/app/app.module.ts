c036c23405f41ba330a0027ef884282b357f09df
Csar Hernndez Rocha <cesarhrocha96@gmail.com>
2019-02-10 22:40:38 -0600

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,66 +1,24 @@
-import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { StatusBar } from '@ionic-native/status-bar';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { Config } from '../config';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { firebaseConfig } from '../config';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { AuthService } from '../services/auth.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxErrorsModule } from '@ultimate/ngxerrors';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Facebook } from '@ionic-native/facebook'
-
-import { ComponentsModule } from '../pages/components/components.module';
-import { GoogleMapsModule } from '../pages/google-maps/google-maps.module';
-import { HomeModule } from '../pages/home/home.module';
-import { SlideBoxModule } from '../pages/slide-box/slide-box.module';
-import { WordpressModule } from '../pages/wordpress/wordpress.module';
-import { MyApp } from './app.component';
 
 @NgModule({
-	declarations: [
-		MyApp,
-		LoginPage,
-		SignupPage
-	],
-	imports: [
-		BrowserModule,
-		HttpModule,
-		IonicModule.forRoot(MyApp),
-		AgmCoreModule.forRoot(),
-		AngularFireModule.initializeApp(firebaseConfig.fire),
-		ComponentsModule,
-		GoogleMapsModule,
-		HomeModule,
-		SlideBoxModule,
-		WordpressModule,
-		FormsModule,
-		ReactiveFormsModule,
-		NgxErrorsModule
-	],
-	bootstrap: [IonicApp],
-	entryComponents: [
-		MyApp,
-		LoginPage,
-		SignupPage
-	],
-	providers: [
-		Config,
-		StatusBar,
-		AuthService,
-		AngularFireAuth,
-		SplashScreen,
-		Facebook
-	]
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
